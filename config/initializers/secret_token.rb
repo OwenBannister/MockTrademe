804424bef44a3eff0ca02ff0e12cda5cbcc7a95e
OwenB <Owen.Bannister@datacom.co.nz>
2012-11-15 12:48:17 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trademe::Application.config.secret_token = 'b51fe0ae24253beab694662332a3c2b180893efa02e018ede11ef317d4448898493199b31f57c3867e00dd4675bb0f1727f60d9bddc5b579c0f0d80241de9829'
